ea7ce2b3f164525dc558347447a0a0bf7783e5fb
Logan Rice <loganrice72@gmail.com>
2014-07-18 11:19:10 -0700

    curtain scroll effect removed smooth scroll

--- js/app.js
+++ js/app.js
@@ -1,57 +1,10 @@
-$(document).ready( function() {
-
-  var setHeight = $( document ).height();
-  $('.page').css("height", setHeight);
-
-  $("#header-nav").on("click", "li", function() {
-    $(this).siblings().removeClass("active");
-    $(this).addClass("active");
-  });
-
-  // ******* Smooth Scroll ****************
-
-   $(".contactLink").click(function(){
-                    if ($("#contactForm").is(":hidden")){
-                        $("#contactForm").slideDown("slow");
-                    }
-                    else{
-                        $("#contactForm").slideUp("slow");
-                    }
-                });
-            });
-            function closeForm(){
-                $("#messageSent").show("slow");
-                setTimeout('$("#messageSent").hide();$("#contactForm").slideUp("slow")', 2000);
-           }
-
 $(document).ready(function() {
-  function filterPath(string) {
-    return string
-      .replace(/^\//,'')
-      .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
-      .replace(/\/$/,'');
-  }
-  $('a[href*=#]').each(function() {
-    if ( filterPath(location.pathname) == filterPath(this.pathname)
-    && location.hostname == this.hostname
-    && this.hash.replace(/#/,'') ) {
-      var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
-      var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
-       if ($target) {
-         var targetOffset = $target.offset().top;
-         $(this).click(function() {
-           $('html, body').animate({scrollTop: targetOffset}, 1000);
-           var d = document.createElement("div");
-        d.style.height = "101%";
-        d.style.overflow = "hidden";
-        document.body.appendChild(d);
-        window.scrollTo(0,scrollToM);
-        setTimeout(function() {
-        d.parentNode.removeChild(d);
-            }, 10);
-           return false;
-         });
-      }
+  $('.curtains').curtain({
+    scrollSpeed: 300,
+    controls: '.menu',
+    curtainLinks: '.curtain-links',
+    nextSlide: function(){
+      console.log("ok");
     }
   });
 });
